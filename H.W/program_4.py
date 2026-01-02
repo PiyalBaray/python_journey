025277a3d3375351e04e2cdddcc5e9d419ab1e27
PiyalBaray <piyalbaray314@gmail.com>
2026-01-02 22:06:19 +0530

    Merge remote-tracking branch 'github_/elif' into elif

---
+++ H.W/program_4.py
@@ -0,0 +1,7 @@
+# program to find the area and perimeter of a rectangle
+a=float(input('Enter length :'))
+b=float(input('Enter Breadth :'))
+area=a*b
+perimeter=2*(a+b)
+print('Area of rectangle is :',area)
+print('perimeter of rectangle is :',perimeter)
