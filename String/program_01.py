3626ba299c3af63898a94f1097938e1cda910a34
PiyalBaray <piyalbaray314@gmail.com>
2026-02-07 19:54:24 +0530

    Add initial string creation examples in program_01.py

---
+++ String/program_01.py
@@ -0,0 +1,9 @@
+# How to creating string
+a="piyal"
+a1="""piyal"""
+a2='''piyal'''
+a3='piyal'
+print('This process 1 :',a)
+print('This process 2 :',a1)
+print('This process 3 :',a2)
+print('This process 4 :',a3)
