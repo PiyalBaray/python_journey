eb018cd1ba787b5fc34134ff7cda37e55561d90d
PiyalBaray <piyalbaray314@gmail.com>
2026-02-10 23:38:42 +0530

    Add examples of string formatting using the format function in program_05.py

---
+++ String/String function/program_05.py
@@ -0,0 +1,11 @@
+# use format function
+name='piyal'
+gender='male'
+
+# process-01
+print('Hi my name is {}and I am a{}'.format(name,gender))
+
+# process-02
+print('Hi my name is {1}and I am a{0}'.format(gender,name))
+
+
