8895c654aace778425cd1c05a24c91b2d8faa973
PiyalBaray <piyalbaray314@gmail.com>
2026-02-11 00:12:45 +0530

    Add examples of string validation functions in program_06.py

---
+++ String/String function/program_06.py
@@ -0,0 +1,20 @@
+a='piyal'
+a1='piyal7533'
+a2='2266'
+
+# use isalnum function
+print(a.isalnum())
+print(a1.isalnum())
+print(a2.isalnum())
+
+# use isalpha function
+print(a.isalpha())
+print(a1.isalpha())
+print(a2.isalpha())
+
+# use isdigit function
+print(a.isdigit())
+print(a1.isdigit())
+print(a2.isdigit())
+
+
