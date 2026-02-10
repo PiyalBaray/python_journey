37c502c52401db31cc9e0363d12cfa3726e16216
PiyalBaray <piyalbaray314@gmail.com>
2026-02-10 22:30:22 +0530

    Add string manipulation examples in program_01.py

---
+++ String/String function/program_01.py
@@ -0,0 +1,15 @@
+# Use len function:
+a='piyalBaray'
+print('Lenght of this string :',len(a))
+# use max function:
+# max value in base of axi value
+print("Max value of this strin :",max(a))
+# use min function
+# min value in base of axi value
+print("Min value of this string :",min(a))
+# use sorted function
+# Ascending value of this string 
+print("ascending value of this string :",sorted(a)) 
+# dictionary value of this string 
+print("ascending value of this string :", sorted(a, reverse=True))
+
