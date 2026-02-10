2c4d63ebadcc8ea80dc309293a678cb2611215c9
PiyalBaray <piyalbaray314@gmail.com>
2026-02-10 22:45:03 +0530

    Add string case manipulation examples in program_02.py

---
+++ String/String function/program_02.py
@@ -0,0 +1,11 @@
+
+a='piyal baray'
+a1='PIYAL BARAY'
+# use capitalize function
+print(a.capitalize())
+# use title function
+print(a.title())
+# use upper function
+print(a.upper ())
+# use lower function
+print(a1.lower  ())
