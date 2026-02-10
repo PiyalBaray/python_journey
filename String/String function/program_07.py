39a157a36db1ca63581c25297ea5e58c597bf1f3
PiyalBaray <piyalbaray314@gmail.com>
2026-02-11 00:18:38 +0530

    Add examples of string split and join functions in program_07.py

---
+++ String/String function/program_07.py
@@ -0,0 +1,12 @@
+a='my name is piyal'
+
+# use split function
+print(a.split())
+
+b=['my', 'name', 'is', 'piyal']
+
+# use joint function
+print(" ".join(b))
+print("_".join(b))
+print("-".join(b))
+print("+".join(b))
