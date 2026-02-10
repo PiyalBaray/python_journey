27395f36699d7b095c9ad6783e34791ca93c72ed
PiyalBaray <piyalbaray314@gmail.com>
2026-02-10 23:09:47 +0530

    Add string functions examples in program_03.py

---
+++ String/String function/program_03.py
@@ -0,0 +1,10 @@
+a='My name is piyal'
+
+# use count function 
+print('In this string i present :',a.count('i'),"times")
+
+#use find function
+print('position of is :',a.find('is'))
+
+# use index function
+print('position of is :',a.index('is'))
