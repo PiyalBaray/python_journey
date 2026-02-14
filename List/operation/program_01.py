3690083bee35bc93c71100279367d237e8c548c6
PiyalBaray <piyalbaray314@gmail.com>
2026-02-14 19:41:10 +0530

    Add program_01.py to demonstrate arithmetic operations with lists

---
+++ List/operation/program_01.py
@@ -0,0 +1,12 @@
+# Arithmetic opration(+,*)
+a=[1,2,4,5,8,2,8,5]
+b=[8,5,4]
+#use "+" oprator
+print("My 1st list",a)
+print("My 2nd list",b)
+print(a+b)
+
+#use "*" oprator
+print(a*3)
+print((a+b)*3)
+
