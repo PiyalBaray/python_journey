7bd8e8b4530d83d02a7360765dec794681ac8465
PiyalBaray <piyalbaray314@gmail.com>
2026-02-14 20:06:31 +0530

    Add program_04.py to demonstrate the use of len(), min(), max(), and sorted() functions on a list

---
+++ List/operation/program_04.py
@@ -0,0 +1,15 @@
+a=[15,5,8,4,8,4,8,2,8,7,4,10,15,14,12,19,16,13]
+
+# How to use len() function
+print('Length of my list :',len(a))
+
+# How to use min() function
+print("Min value of my list :",min(a))
+
+# How to use max() function
+print("Max value of my list :",max(a))
+
+# How to use sorted() function
+print("Increase vale of my list :",sorted(a))
+print("decrease vale of my list :",sorted(a,reverse=True))
+
