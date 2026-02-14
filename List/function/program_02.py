41e1a4b892f6a970c481757b07d079fff8c80baf
PiyalBaray <piyalbaray314@gmail.com>
2026-02-14 20:16:42 +0530

    Add program_02.py to demonstrate the use of count() function on a list

---
+++ List/function/program_02.py
@@ -0,0 +1,5 @@
+# how to use count () function
+a=[48,8,4,84,8,4,8,4,84,84,848,84]
+print(" 4 present in my list :",a.count(4),"times")
+print(" 100 present in my list :",a.count(100),"times")
+print(" 84 present in my list :",a.count(84),"times")
