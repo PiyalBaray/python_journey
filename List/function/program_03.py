2a2cd6ff41a791b6fe6f3e4bf344d67171ca72ea
PiyalBaray <piyalbaray314@gmail.com>
2026-02-15 11:07:28 +0530

    Add program_03.py to demonstrate the use of index() function on a list

---
+++ List/function/program_03.py
@@ -0,0 +1,6 @@
+a=[5,4,8,1,81,3,9,44,55,66,58,42]
+print('My list is :',a)
+# How to use index() function
+print("Index of 42 :",a.index(42))
+print("Index of 5 :",a.index(5))
+print("Index of 44 :",a.index(44))
